11a8857a7f9baf353930508e0c72104d7fc204d0
xieyou0608 <b07705003@ntu.edu.tw>
2023-02-22 21:56:44 +0800

    test: Add loginForm click test
    想要測試點擊 login 有沒有觸發 preventDefault，網路上找不太到好的解
    用 userEvent click 時如果沒有 preventDefault 會報錯
    不過測試還是會通過，可能要再做進一步 search 或思考測試方向
    譬如測試送出表單可能會發生的行為
    
    另外這裡 mock dispatch 來讓測試更單純，但這樣沒辦法測到 authApi 的改動
    如果想測試點下登入後會跑出登入中的字樣
    可能是
    1) 功能設計有問題，很多動作在 RTK 導致難以追蹤測試
    2) 直接寫整合測試，不 mock dispatch 而是 mock AuthService
       只 mock 打外部 API 的動作並模擬回傳資料

--- src/components/Auth/LoginForm.test.js
+++ src/components/Auth/LoginForm.test.js
@@ -4,9 +4,18 @@ import userEvent from "@testing-library/user-event";
 
 import LoginForm from "./LoginForm";
 
-describe("LoginForm features unit test", () => {
+const mockDispatch = jest.fn();
+jest.mock("react-redux", () => {
+  return {
+    ...jest.requireActual("react-redux"),
+    useDispatch: () => mockDispatch,
+  };
+});
+
+describe("LoginForm unit test", () => {
   test("the login form is display correctly", () => {
     renderWithProviders(<LoginForm />);
+
     const emailInput = screen.getByLabelText("信箱");
     const passwordInput = screen.getByLabelText("密碼");
     const loginBtn = screen.getByRole("button", { name: "登入" });
@@ -18,5 +27,17 @@ describe("LoginForm features unit test", () => {
     expect(passwordInput).toBeInTheDocument();
     expect(loginBtn).toBeInTheDocument();
     expect(goToRegisterLink).toBeInTheDocument();
+  });
+
+  test("click loginBtn will trigger dispatch", () => {
+    renderWithProviders(<LoginForm />);
+
+    const loginBtn = screen.getByRole("button", { name: "登入" });
+    userEvent.click(loginBtn);
+
+    expect(mockDispatch).toHaveBeenCalled();
+
+    // 如果沒有寫 preventDefault, test log 會報錯，但還是會通過測試
+    // expect(submitEvent.preventDefault).toHaveBeenCalled();
   });
 });
